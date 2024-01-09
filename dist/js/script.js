270ad5cd2f97b102159bfa28e9cb45c44ecde3c2
zaldyprtm <zaldyprtm@gmail.com>
2024-01-09 11:46:28 +0700

    dark mode

--- dist/js/script.js
+++ dist/js/script.js
@@ -24,3 +24,23 @@ hamburger.addEventListener('click', function( ) {
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
 });
+
+// klik diluar hamburger
+window.addEventListener('click', function(e){
+    if(e.target != hamburger && e.target != navMenu) {
+        hamburger.classList.remove('hamburger-active');
+        navMenu.classList.add('hidden');
+    }
+});
+
+//Dark Mode
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', function() {
+    if (darkToggle.checked) {
+        html.classList.add('dark');
+    } else  {
+        html.classList.remove('dark');
+    }
+});
