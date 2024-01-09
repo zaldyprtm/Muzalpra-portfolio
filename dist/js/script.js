622282ee475d565ee3fe0fe40a72dac49602824b
zaldyprtm <zaldyprtm@gmail.com>
2024-01-09 11:00:16 +0700

    updatescript.js

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,12 +2,16 @@
 window.onscroll = function()    {
     const header = document.querySelector('header');
     const fixedNav = header.offsetTop;
+    const toTop = document.querySelector('#ttop');
 
     if(window.pageYOffset > fixedNav)  {
         header.classList.add('navbar-fixed');
-
+        toTop.classList.remove('hidden');
+        toTop.classList.add('flex');
     }else{
         header.classList.remove('navbar-fixed');
+        toTop.classList.remove('flex');
+        toTop.classList.add('hidden');
     }
 }
 
