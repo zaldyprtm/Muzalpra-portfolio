22e344ceb9485b1d59503f7146cb8e937d6e7834
zaldyprtm <zaldyprtm@gmail.com>
2024-01-09 14:15:30 +0700

    tambah fitur dark mode dan lainnya

--- dist/js/script.js
+++ dist/js/script.js
@@ -40,7 +40,18 @@ const html = document.querySelector('html');
 darkToggle.addEventListener('click', function() {
     if (darkToggle.checked) {
         html.classList.add('dark');
+        localStorage.theme = 'dark';
     } else  {
         html.classList.remove('dark');
+        localStorage.theme = 'light';
     }
 });
+
+
+//posisi toggle pindah
+// On page load or when changing themes, best to add inline in `head` to avoid FOUC
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    darkToggle.checked = true;
+  } else {
+    darkToggle.checked = false;
+  }
